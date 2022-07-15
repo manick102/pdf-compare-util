ccf5117cb560e6d75d43bfdfbd25f24423927f8a
Manickavasagam Dakshinamoorthy <manick.dk@gmail.com>
2022-07-15 13:31:52 +0800

    test

--- src/main/java/comparison/PDFComparison.java
+++ src/main/java/comparison/PDFComparison.java
@@ -1,6 +1,5 @@
 package comparison;
 
-//import de.redsix.pdfcompare.PdfComparator;
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
 import com.juliusbaer.itasia.tes.pdfcompare.PdfComparator;
 
@@ -9,12 +8,12 @@ import java.io.IOException;
 public class PDFComparison {
 
     public static void main(String[] arggs) throws IOException {
-        new PdfComparator("E:\\automation\\pdfcompare-master\\src\\main\\java\\comparison\\MergedTwoFiles.pdf", "E:\\automation\\pdfcompare-master\\src\\main\\java\\comparison\\C_MD72363244-68283838C.pdf").compare().writeTo("diffOutput");
+        new PdfComparator("E:\\automation\\pdf-compare-util-develop\\src\\main\\java\\comparison\\A_MD72363244-78283838X.pdf", "E:\\automation\\pdfcompare-master\\src\\main\\java\\comparison\\C_MD72363244-68283838C.pdf").compare().writeTo("diffOutput");
 
-        PDFMergerUtility ut = new PDFMergerUtility();
-        ut.addSource("E:\\automation\\pdfcompare-master\\src\\main\\java\\comparison\\A_MD72363244-78283838X.pdf");
-        ut.addSource("E:\\automation\\pdfcompare-master\\src\\main\\java\\comparison\\B_MD72363244-88283838Y.pdf");
-        ut.setDestinationFileName("E:\\automation\\pdfcompare-master\\src\\main\\java\\comparison\\MergedTwoFiles.pdf");
-        ut.mergeDocuments();
+//        PDFMergerUtility ut = new PDFMergerUtility();
+//        ut.addSource("E:\\automation\\pdf-compare-util-develop\\src\\main\\java\\comparison\\A_MD72363244-78283838X.pdf");
+//        ut.addSource("E:\\automation\\pdf-compare-util-develop\\src\\main\\java\\comparison\\B_MD72363244-88283838Y.pdf");
+//        ut.setDestinationFileName("E:\\automation\\pdf-compare-util-develop\\src\\main\\java\\comparison\\AB_Merged.pdf");
+//        ut.mergeDocuments();
     }
 }
