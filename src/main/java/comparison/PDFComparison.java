1584adbedadd4265605dee5bb9f6b79f4b4047c8
arunmech28 <38682818+arunmech28@users.noreply.github.com>
2022-07-19 21:40:12 +0530

    changes related to excel

--- src/main/java/comparison/PDFComparison.java
+++ src/main/java/comparison/PDFComparison.java
@@ -2,13 +2,27 @@ package comparison;
 
 //import de.redsix.pdfcompare.PdfComparator;
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
+
+import com.juliusbaer.itasia.tes.pdfcompare.CompareResult;
 import com.juliusbaer.itasia.tes.pdfcompare.PdfComparator;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.poi.common.usermodel.HyperlinkType;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFHyperlink;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 public class PDFComparison {
 
@@ -37,6 +51,21 @@ public class PDFComparison {
 //        ut.addSource("E:\\automation\\pdfcompare-master\\src\\main\\java\\comparison\\B_MD72363244-88283838Y.pdf");
 //        ut.setDestinationFileName("E:\\automation\\pdfcompare-master\\src\\main\\java\\comparison\\MergedTwoFiles.pdf");
 //        ut.mergeDocuments();
+        
+        /* Creating excel workbook and column header */
+    	XSSFWorkbook wb = new XSSFWorkbook(); 
+    	XSSFSheet sheet = wb.createSheet("Sheet1"); 
+    	int rownum = 0;
+    	XSSFRow rowhead = sheet.createRow(rownum);  
+    	rowhead.createCell(0).setCellValue("S.No.");  
+    	rowhead.createCell(1).setCellValue("T24 File1");  
+    	rowhead.createCell(2).setCellValue("T24 File2");  
+    	rowhead.createCell(3).setCellValue("T24 Merged File");  
+    	rowhead.createCell(4).setCellValue("TAP File");  
+    	rowhead.createCell(5).setCellValue("Status");  
+    	rowhead.createCell(6).setCellValue("Result");  
+    	rownum++;
+    	/* Creating excel workbook and column header */
 
         File[] beforeFiles = readBeforeFiles();
         for (File file : readTapFiles()) {
@@ -65,13 +94,47 @@ public class PDFComparison {
                 ut.setDestinationFileName("/Users/arunpandian/IdeaProjects/pdf-compare-util/src/main/resources/after/"+MERGED_FILE_PREFIX + identifier + ".pdf");
                 ut.mergeDocuments();
 
-                new PdfComparator(file.getAbsolutePath(),
+                // new PdfComparator(file.getAbsolutePath(),
+                   //     "/Users/arunpandian/IdeaProjects/pdf-compare-util/src/main/resources/after/"+MERGED_FILE_PREFIX + identifier + ".pdf")
+                     //   .compare()
+                       // .writeTo("/Users/arunpandian/IdeaProjects/pdf-compare-util/src/main/resources/results/"+ RESULT_FILE_PREFIX+ identifier + ".pdf");
+                
+                /* excel related changes */
+                CompareResult result = new PdfComparator(file.getAbsolutePath(),
                         "/Users/arunpandian/IdeaProjects/pdf-compare-util/src/main/resources/after/"+MERGED_FILE_PREFIX + identifier + ".pdf")
-                        .compare()
-                        .writeTo("/Users/arunpandian/IdeaProjects/pdf-compare-util/src/main/resources/results/"+ RESULT_FILE_PREFIX+ identifier + ".pdf");
+                        .compare();
+                result.writeTo("/Users/arunpandian/IdeaProjects/pdf-compare-util/src/main/resources/results/"+ RESULT_FILE_PREFIX+ identifier );
+                
+                
+                CreationHelper createHelper = wb.getCreationHelper();
+                XSSFRow row = sheet.createRow(rownum);  
+                row.createCell(0).setCellValue(""+rownum);  
+                row.createCell(1).setCellValue(astA.getName());  
+                row.createCell(2).setCellValue(secB.getName());  
+                row.createCell(3).setCellValue(T24_SECB_REM_PAGE_1_PREFIX+ identifier);  
+            	row.createCell(4).setCellValue(file.getName());  
+            	row.createCell(5).setCellValue(result.isEqual() ? "PASS": "FAIL"); 
+            	
+            	XSSFHyperlink link = (XSSFHyperlink)createHelper.createHyperlink(HyperlinkType.FILE);
+            	 XSSFCellStyle hlinkstyle = wb.createCellStyle();
+                 XSSFFont hlinkfont = wb.createFont();
+                 hlinkfont.setUnderline(XSSFFont.U_SINGLE);
+                 hlinkfont.setColor(IndexedColors.BLUE.index);
+                 hlinkstyle.setFont(hlinkfont);
+                 
+            	XSSFCell cell= row.createCell(6);
+            	cell.setCellValue("Result File Link");
+            	link.setAddress(RESULT_FILE_PATH + RESULT_FILE_PREFIX+ identifier + ".pdf");
+            	cell.setHyperlink(link);
+                cell.setCellStyle(hlinkstyle);
+            	
+            	/* excel related changes */
             }
         }
-
+        String fileName = "Results" + new SimpleDateFormat("yyyyMMddHHmm").format(new Date()) + ".xlsx";
+        FileOutputStream out = new FileOutputStream(new File(fileName));
+        wb.write(out);
+        out.close();
     }
 
     public static File[] readTapFiles() {
